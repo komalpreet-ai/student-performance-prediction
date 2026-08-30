abf28674187de8c44a78442f791720b9886a440a
komalpreet-ai <komalpreetkaur4847@gmail.com>
2026-08-30 14:34:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print(data)
 # Display basic information
 print("\nDataset Information:")
 print(data.info())
+
 # Show first 5 students
 print("\nFirst 5 rows:")
 print(data.head())
@@ -20,3 +21,13 @@ print(data.columns)
 # Show basic statistics
 print("\nStatistics:")
 print(data.describe())
+
+# Separate input features and target
+X = data[["Study_Hours", "Attendance", "Previous_Marks", "Assignments"]]
+y = data["Final_Marks"]
+
+print("\nInput Features:")
+print(X)
+
+print("\nTarget:")
+print(y)
